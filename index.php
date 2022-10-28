8100ab4714b6d5e517840d2f3c7b51812439d26f
efecan-kutluk <efecankutluk@gmail.com>
2022-10-28 23:17:31 +0300

    css & js refresh

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="Content-Security-Policy" content="default-src 'self';">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>When the h*** are you?!</title>
-    <link rel="stylesheet" href="./style.css">
-    <script src="./app.js?v=101" defer></script>
+    <link rel="stylesheet" href="./style.css?v=100">
+    <script src="./app.js?v=102" defer></script>
 </head>
 <body>
     <div class="txt-speechbox">
