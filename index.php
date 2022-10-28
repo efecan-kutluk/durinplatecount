e41a97516ba3b6f51b859e1249f15556642317d2
efecan-kutluk <efecankutluk@gmail.com>
2022-10-29 00:21:40 +0300

    css: added Skranji font-family, fixed border for txt-speech, final tweaks in speech position

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta http-equiv="Content-Security-Policy" content="default-src 'self';">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>When the h*** are you?!</title>
-    <link rel="stylesheet" href="./style.css?v=102">
+    <link rel="stylesheet" href="./style.css?v=105">
     <script src="./app.js?v=102" defer></script>
 </head>
 <body>
     <div class="txt-speechbox">
-	  <div class="txt-protest">IT HAS BEEN <span id="time"></span> DAYS!</div>
+	  <div class="txt-protest">IT HAS BEEN <span id="time"></span> DAYS !</div>
 	  <div class="txt-protest">
-		WHERE IS MY <img id="plate" src="durins_plate.jpg"/> !?
+		WHERE IS MY <img id="plate" src="durins_plate.jpg"/> ?!
 	  </div>
     </div>
     <div id="root">
